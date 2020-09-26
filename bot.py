26b29b5d71c11b57868be16554cfe5cf0e98ed3b
Ryan Bender <ryanbender65@yahoo.com>
2020-09-26 14:27:19 -0400

    added project

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord import channel
+from discord.message import Message
+
+class MyClient(discord.Client):
+    TOKEN = "NzU5NDY1OTc1NjQ4MzU0MzM1.X295zg._UU-sU5wGQgQplIEQTKusSWvJ7k"
+
+    async def on_ready(self) -> None:
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message) -> None:
+        channel = message.channel
+
+        print(message.author)
+
+        if str(message.author) != "Tournament Bot#8806" and message.content == "Bot, send Gabe a message!":
+            await channel.send("Bro its insane how easy this thing is to get up and running. This is running in Python.")
+
+
+
+client = MyClient()
+client.run(client.TOKEN)
